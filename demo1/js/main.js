ab477e814973bd016674f1c39ab488aea09da28a
muzi1102 <2573504171@qq.com>
2016-09-13 16:19:33 +0800

    update

--- demo1/js/main.js
+++ demo1/js/main.js
@@ -54,8 +54,10 @@ $(function() {
         })
     });
     // 搜索猜你喜欢问题展开；
-    (function() {
-        $('.ui-proitem').each(function() {
+    // (function() {
+        
+    // })()
+    $('.ui-proitem').each(function() {
             var proH = $(this).height();
             var colH=$(this).find('li').height()*3+3;
             if (proH > colH) {
@@ -81,7 +83,6 @@ $(function() {
                 })
             }
         })
-    })()
     // 清空历史记录
     $('.ui-empty').on('click',function(){
         $(this).parents('.ui-related-list').remove();
